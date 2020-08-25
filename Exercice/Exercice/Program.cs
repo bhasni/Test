29b4331b1b59f2680ec12ec06512b50ff1f2eeaf
bhasni <BHASNI@externe.henner.fr>
2020-08-25 10:10:27 +0200

    Premier Projet

---
+++ Exercice/Exercice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
